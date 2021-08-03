ffd8c6cada0283911071bf3f60b7d2456f70b1af
mdwiltfong <wiltfong.michael1@gmail.com>
2021-08-03 15:50:42 -0400

    setup makeMeme function

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let img_link=document.querySelector(`#URL`);
 let top=document.querySelector(`#top`);
 let bottom=document.querySelector(`#bottom`);
 
-function makeMeme(btm,top,img){
+function makeMeme(btm,tp,img){
 
 
 }
