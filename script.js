7c0fde0db19dd4ca5a8345800500f847eb496610
mdwiltfong <wiltfong.michael1@gmail.com>
2021-08-03 04:46:47 -0400

    meme page is stylized

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 let gen_btn=document.querySelector("button");
 let btm_text=document.querySelector(`input[name="Bottom Text]`);
 let tp_text=document.querySelector(`input[name="Top Text]`);
+let img_link=document.querySelector(`#URL`);
+let top=document.querySelector(`#top`);
+let bottom=document.querySelector(`#bottom`);
+
+function makeMeme(btm,top,img){
+
+
+}
 
