74eef467df2b263ad9267ce90feb00b62e463782
mdwiltfong <wiltfong.michael1@gmail.com>
2021-08-03 21:53:15 -0400

    editting JS

--- script.js
+++ script.js
@@ -2,10 +2,31 @@ let gen_btn=document.querySelector("button");
 let btm_text=document.querySelector(`input[name="Bottom Text]`);
 let tp_text=document.querySelector(`input[name="Top Text]`);
 let img_link=document.querySelector(`#URL`);
-let top=document.querySelector(`#top`);
-let bottom=document.querySelector(`#bottom`);
+let form = document.querySelector(`form`);
 
-function makeMeme(btm,tp,img){
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    storeMeme(img_link.value,)
+    console.log(`submit`)
+})
+
+
+
+
+
+function storeMeme(url,top,bottom){
+let data={
+    url:url,
+    top:top,
+    bottom:bottom,
+}
+try{
+    
+    sessionStorage(data);
+    console.log(`Stored`)
+}catch(error){
+    console.log(error)
+}
 
 
 }
