92056a02d11143f940848ce628a4e4caf6d82a55
mdwiltfong <wiltfong.michael1@gmail.com>
2021-08-05 21:50:29 -0400

    CORB response from browser Fixes #2

--- script.js
+++ script.js
@@ -3,33 +3,22 @@ let btm_text=document.getElementById(`bottom_text`);
 let tp_text=document.getElementById(`top_text`);
 let img_link=document.querySelector(`#URL`);
 let form = document.querySelector(`form`);
+let input= document.getElementById(`input_field`);
+let form_meme = document.getElementById(`form_meme`);
+let meme= form_meme.querySelectorAll(`div`);
+let tp =meme[0].children[0];
+let img =meme[0].children[1];
+let bt =meme[0].children[2];
 
 form.addEventListener('submit',(e)=>{
-    
-    storeMeme(img_link.value,tp_text.value,btm_text.value)
-  
+    e.preventDefault();
+    input.style.display=`none`
+    form_meme.style.display=`block`
+    tp.innerText=tp_text.value;
+    bt.innerText=btm_text.value;  
+    img.setAttribute('src',`${img_link.value}`)  
 })
 
 
 
-
-
-
-
-function storeMeme(url,top,bottom){
-let data={
-    url:url,
-    top:top,
-    bottom:bottom,
-}
-try{
-    
-    localStorage.setItem(`meme`,JSON.stringify(data));
-    console.log(`Stored`)
-}catch(error){
-    console.log(error)
-}
-
-
-}
 
