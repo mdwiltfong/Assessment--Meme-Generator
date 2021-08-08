3e63117da4588ec14b56311a736e26d0b8c8d4e4
mdwiltfong <wiltfong.michael1@gmail.com>
2021-08-08 04:19:41 -0400

    localStorage saves and uploads recently made memes

--- script.js
+++ script.js
@@ -1,49 +1,69 @@
-let gen_btn=document.querySelector("button");
-let btm_text=document.getElementById(`bottom_text`);
-let tp_text=document.getElementById(`top_text`);
+let gen_btn = document.querySelector("button");
+let btm_text = document.getElementById(`bottom_text`);
+let tp_text = document.getElementById(`top_text`);
 let form = document.getElementById(`form`);
-let form_meme=document.querySelector(`#form_meme`);
-let meme_select=document.getElementById(`meme_select`);
-let img=document.getElementById(`meme`);
-let topText=document.getElementById(`top`);
-let bottom=document.getElementById(`bottom`);
-form.addEventListener('submit',(e)=>{
+let form_meme = document.querySelector(`#form_meme`);
+let meme_select = document.getElementById(`meme_select`);
+let img = document.getElementById(`meme`);
+let topText = document.getElementById(`top`);
+let bottom = document.getElementById(`bottom`);
+let last_four = document.getElementById(`last_four`);
+let recImages = document.getElementById(`images`);
+let datas = allStorage();
+console.log(datas);
+let container = document.getElementById(`images`);
+console.log(container);
+let i = localStorage.length;
+form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    if(meme_select.value && btm_text.value && tp_text.value){
-        img.setAttribute('src',`./assets/${meme_select.value}`);
+    if (meme_select.value && btm_text.value && tp_text.value) {
+        img.setAttribute('src', `./assets/${meme_select.value}`);
         console.log(img.getAttribute(`src`))
-        form.style.display='none';
-        form_meme.style.display=`block`;
-        bottom.innerText=btm_text.value;
-        topText.innerText=tp_text.value;
-    }else{
+        form.style.display = 'none';
+        form_meme.style.display = `block`;
+        bottom.innerText = btm_text.value;
+        topText.innerText = tp_text.value;
+        let newMeme = new Meme(meme_select.value, topText.innerText, bottom.innerText, i);
+        console.log(newMeme);
+        localStorage.setItem(`meme${i+1}`, JSON.stringify(newMeme));
+    } else {
         alert(`Please fill out the form to continue!`)
     }
-    
+
+})
+
+window.addEventListener(`DOMContentLoaded`, () => {
+    if (!datas) {
+        console.log(`Waiting for bodacious meme`)
+    }
+  
+for(let j=0;j<i;j++){
+    let img = document.createElement(`img`);
+    img.setAttribute(`src`,`./assets/${JSON.parse(datas[j]).path}`)
+    container.append(img);
+}
+
 
 })
 
 
 
-
-
-
-
-function storeMeme(url,top,bottom){
-let data={
-    url:url,
-    top:top,
-    bottom:bottom,
-}
-try{
-    
-    localStorage.setItem(`meme`,JSON.stringify(data));
-    console.log(`Stored`)
-}catch(error){
-    console.log(error)
+function Meme(url, top, bottom, index) {
+    this.path = url,
+        this.topText = top,
+        this.bottomText = bottom
 }
 
+function allStorage() {
 
+    var values = [],
+        keys = Object.keys(localStorage),
+        i = keys.length;
+
+    while (i--) {
+        values.push(localStorage.getItem(keys[i]));
+    }
+
+    return values;
 }
-
