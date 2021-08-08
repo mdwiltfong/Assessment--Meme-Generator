ae8dd7d2342bffea94b334456fd5a35dfc34ea6b
mdwiltfong <wiltfong.michael1@gmail.com>
2021-08-08 00:47:42 -0400

    users can now select base meme from drop down

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let gen_btn=document.querySelector("button");
 let btm_text=document.getElementById(`bottom_text`);
 let tp_text=document.getElementById(`top_text`);
-let img_link=document.querySelector(`#URL`);
 let form = document.getElementById(`form`);
 let form_meme=document.querySelector(`#form_meme`);
+let meme_select=document.getElementById(`meme_select`);
+let img=document.getElementById(`meme`);
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
-    storeMeme(img_link.value,tp_text.value,btm_text.value);
+    //storeMeme(img_link.value,tp_text.value,btm_text.value);
+    img.setAttribute('src',`./assets/${meme_select.value}`);
+    console.log(img.getAttribute(`src`))
     form.style.display='none';
     form_meme.style.display=`block`;
 })
